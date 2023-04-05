31f2615ef1e6f9743547835d5bf05dd6f8163dd7
bulk <harryhamlin@gmail.com>
2023-04-05 16:15:22 -0700

    initialized app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+    // We declare a state variable that is an array called `issues` and a function to update it.
+  
+    return (
+      <div>
+        {/* Here we pass our getRepoIssues function as a prop to SearchBar */}
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <h2>i like toitles</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default App;
