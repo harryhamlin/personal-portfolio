70834c0abe5905d3defee485e2e01a9e74bd6eae
bulk <harryhamlin@gmail.com>
2023-04-05 22:28:14 -0700

    deployment edit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React//, { useState, useEffect } 
+from 'react';
 
 function App() {
     // We declare a state variable that is an array called `issues` and a function to update it.
