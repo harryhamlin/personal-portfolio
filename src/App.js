ce2bfe2398f779973580f8765641bd141d19e76f
bulk <harryhamlin@gmail.com>
2023-04-06 13:28:07 -0700

    added components and bulma

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React//, { useState, useEffect } 
 from 'react';
+import 'bulma/css/bulma.min.css';
 
 function App() {
     // We declare a state variable that is an array called `issues` and a function to update it.
@@ -10,7 +11,7 @@ function App() {
         <div className="ui grid">
           <div className="ui row">
             <div className="eleven wide column">
-              <h2>i like toitles</h2>
+              <h1 className="is-size-1">i like toitles</h1>
             </div>
           </div>
         </div>
